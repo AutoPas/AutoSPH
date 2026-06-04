4476bb27687cad0f019d5efa577a3c73ce4c09a4
inigoruiz <inigo.ruiz@tum.de>
2026-06-04 18:02:58 +0200

    Initialized particles and output their positions

--- SPHParticle.cpp
+++ SPHParticle.cpp
@@ -30,6 +30,212 @@ class SPHParticle : public autopas::ParticleBaseFP64 {
         _dt(0.),
         _vel_half{0., 0., 0.},
         _eng_half(0.) {}
+
+  /**
+   * Constructor of the SPHParticle class.
+   * @param r position of the particle
+   * @param v velocity of the particle
+   * @param id id of the particle. This id should be unique
+   * @param mass mass of the particle
+   * @param smth smoothing length of the particle
+   * @param snds speed of sound (SouND Speed)
+   */
+  SPHParticle(const std::array<double, 3> &r, const std::array<double, 3> &v, unsigned long id, double mass,
+              double smth, double snds)
+      : autopas::ParticleBaseFP64(r, v, id),
+        _density(0.),
+        _pressure(0.),
+        _mass(mass),
+        _smth(smth),
+        _snds(snds),
+        // temporaries / helpers
+        _v_sig_max(0.),
+        _acc{0., 0., 0.},
+        _energy_dot(0.),
+        _energy(0.),
+        _dt(0.),
+        _vel_half{0., 0., 0.},
+        _eng_half(0.) {}
+
+  /**
+   * Destructor of the SPHParticle
+   */
+  ~SPHParticle() override = default;
+
+  /**
+   * Attribute names for the soa arrays
+   */
+  enum AttributeNames : int {
+    ptr,
+    mass,
+    posX,
+    posY,
+    posZ,
+    smth,
+    density,
+    velX,
+    velY,
+    velZ,
+    soundSpeed,
+    pressure,
+    vsigmax,
+    accX,
+    accY,
+    accZ,
+    engDot,
+    ownershipState
+  };
+
+  double getMass() const { return _mass; }
+
+  double getSmoothingLength() const { return _smth; }
+
+  double getDensity() const { return _density; }
+
+  double getSoundSpeed() const { return _snds; }
+
+  double getPressure() const { return _pressure; }
+
+  double getVSigMax() const { return _v_sig_max; }
+
+  const std::array<double, 3> &getAcceleration() const { return _acc; }
+
+  double getEngDot() const { return _energy_dot; }
+
+  void setDensity(double density) { _density = density; }
+
+  void setEnergy(double energy) { _energy = energy; }
+
+  void calcPressure() {
+    const double hcr = 1.4;
+    _pressure = (hcr - 1.0) * _density * _energy;
+    _snds = sqrt(hcr * _pressure / _density);
+  }
+
+  /**
+   * SoA arrays type, cf. AttributeNames
+   */
+  using SoAArraysType = autopas::utils::SoAType<SPHParticle *,
+                                                double,  // mass
+                                                double,  // posX
+                                                double,  // posY
+                                                double,  // posZ
+                                                double,  // smth
+                                                double,  // density
+                                                double,  // velX
+                                                double,  // velY
+                                                double,  // velZ
+                                                double,  // soundSpeed
+                                                double,  // pressure
+                                                double,  // vsigmax
+                                                double,  // accX
+                                                double,  // accY
+                                                double,  // accZ
+                                                double,  // engDot
+                                                autopas::OwnershipState>::Type;
+
+  /**
+   * Non-const getter for the pointer of this object.
+   * @tparam attribute Attribute name.
+   * @return this.
+   */
+  template <AttributeNames attribute, std::enable_if_t<attribute == AttributeNames::ptr, bool> = true>
+  constexpr typename std::tuple_element<attribute, SoAArraysType>::type::value_type get() {
+    return this;
+  }
+
+  /**
+   * Getter, which allows access to an attribute using the corresponding attribute name (defined in AttributeNames).
+   * @tparam attribute Attribute name.
+   * @return Value of the requested attribute.
+   */
+  template <AttributeNames attribute, std::enable_if_t<attribute != AttributeNames::ptr, bool> = true>
+  constexpr typename std::tuple_element<attribute, SoAArraysType>::type::value_type get() const {
+    if constexpr (attribute == AttributeNames::mass) {
+      return getMass();
+    } else if constexpr (attribute == AttributeNames::posX) {
+      return getR()[0];
+    } else if constexpr (attribute == AttributeNames::posY) {
+      return getR()[1];
+    } else if constexpr (attribute == AttributeNames::posZ) {
+      return getR()[2];
+    } else if constexpr (attribute == AttributeNames::smth) {
+      return getSmoothingLength();
+    } else if constexpr (attribute == AttributeNames::density) {
+      return getDensity();
+    } else if constexpr (attribute == AttributeNames::velX) {
+      return getV()[0];
+    } else if constexpr (attribute == AttributeNames::velY) {
+      return getV()[1];
+    } else if constexpr (attribute == AttributeNames::velZ) {
+      return getV()[2];
+    } else if constexpr (attribute == AttributeNames::soundSpeed) {
+      return getSoundSpeed();
+    } else if constexpr (attribute == AttributeNames::pressure) {
+      return getPressure();
+    } else if constexpr (attribute == AttributeNames::vsigmax) {
+      return getVSigMax();
+    } else if constexpr (attribute == AttributeNames::accX) {
+      return getAcceleration()[0];
+    } else if constexpr (attribute == AttributeNames::accY) {
+      return getAcceleration()[1];
+    } else if constexpr (attribute == AttributeNames::accZ) {
+      return getAcceleration()[2];
+    } else if constexpr (attribute == AttributeNames::engDot) {
+      return getEngDot();
+    } else if constexpr (attribute == AttributeNames::ownershipState) {
+      return this->_ownershipState;
+    } else {
+      autopas::utils::ExceptionHandler::exception("SPHParticle::get: unknown attribute");
+    }
+  }
+
+  /**
+   * Setter, which allows set an attribute using the corresponding attribute name (defined in AttributeNames).
+   * @tparam attribute Attribute name.
+   * @param value New value of the requested attribute.
+   */
+  template <AttributeNames attribute>
+  constexpr void set(typename std::tuple_element<attribute, SoAArraysType>::type::value_type value) {
+    if constexpr (attribute == AttributeNames::mass) {
+      setMass(value);
+    } else if constexpr (attribute == AttributeNames::posX) {
+      _r[0] = value;
+    } else if constexpr (attribute == AttributeNames::posY) {
+      _r[1] = value;
+    } else if constexpr (attribute == AttributeNames::posZ) {
+      _r[2] = value;
+    } else if constexpr (attribute == AttributeNames::smth) {
+      setSmoothingLength(value);
+    } else if constexpr (attribute == AttributeNames::density) {
+      setDensity(value);
+    } else if constexpr (attribute == AttributeNames::velX) {
+      _v[0] = value;
+    } else if constexpr (attribute == AttributeNames::velY) {
+      _v[1] = value;
+    } else if constexpr (attribute == AttributeNames::velZ) {
+      _v[2] = value;
+    } else if constexpr (attribute == AttributeNames::soundSpeed) {
+      setSoundSpeed(value);
+    } else if constexpr (attribute == AttributeNames::pressure) {
+      setPressure(value);
+    } else if constexpr (attribute == AttributeNames::vsigmax) {
+      setVSigMax(value);
+    } else if constexpr (attribute == AttributeNames::accX) {
+      _acc[0] = value;
+    } else if constexpr (attribute == AttributeNames::accY) {
+      _acc[1] = value;
+    } else if constexpr (attribute == AttributeNames::accZ) {
+      _acc[2] = value;
+    } else if constexpr (attribute == AttributeNames::engDot) {
+      setEngDot(value);
+    } else if constexpr (attribute == AttributeNames::ownershipState) {
+      _ownershipState = value;
+    } else {
+      autopas::utils::ExceptionHandler::exception("SPHParticle::set: unknown attribute");
+    }
+  }
+
  private:
   double _density;   // density
   double _pressure;  // pressure
--- main.cpp
+++ main.cpp
@@ -19,13 +19,54 @@ using AutoPasContainer = autopas::AutoPas<Particle>;
 
 // using AutoPasContainer = autopas::AutoPas<autopas::ParticleBaseFP64>;
 
-int main() {
+void SetupIC(AutoPasContainer &sphSystem, double *end_time, const std::array<double, 3> &bBoxMax) {
+  // Place SPH particles
+  AutoPasLog(INFO, "Setup started");
 
+  const double dx = 1.0 / 2.0;
+  unsigned int i = 0;
+  for (double x = 0; x < bBoxMax[0]; x += dx) {         // NOLINT
+    for (double y = 0; y < bBoxMax[1]; y += dx) {       // NOLINT
+      for (double z = 0; z < bBoxMax[2]; z += dx) {     // NOLINT
+        Particle ith({x, y, z}, {0, 0, 0}, i++, 0.75, 0.012, 0.);
+        ith.setDensity(1.0);
+        ith.setEnergy(2.5);
+        sphSystem.addParticle(ith);
+      }
+    }
+  }
+
+  // Set the end time
+  *end_time = .012;
+
+  AutoPasLog(INFO, "Setup completed");
+  AutoPasLog(INFO, "Number of particles: {}", i);
+}
+
+void Initialize(AutoPasContainer &sphSystem) {
+  AutoPasLog(INFO, "Initialization started");
+  for (auto part = sphSystem.begin(autopas::IteratorBehavior::owned); part.isValid(); ++part) {
+    part->calcPressure();
+  }
+  AutoPasLog(INFO, "Initialization completed");
+}
+
+void LogParticlePositions(AutoPasContainer &sphSystem) {
+  std::cout << "initialize... completed" << std::endl;
+  std::array<double, 3> position;
+  for (auto part = sphSystem.begin(autopas::IteratorBehavior::owned); part.isValid(); ++part) {
+    // std::cout << part->getMass() << std::endl;
+    position = part->getR();
+    AutoPasLog(INFO, "Position of particle {}: {}, {}, {}", part->getID(), position[0], position[1], position[2]);
+  }
+  std::cout << "initialize... completed" << std::endl;
+}
+
+int main() {
   std::cout<< "Hello from SPH world!\n";
 
   std::array<double, 3> boxMin({0., 0., 0.}), boxMax{};
-  boxMax[0] = 1.;
-  boxMax[1] = boxMax[2] = boxMax[0] / 8.0;
+  boxMax[0] = boxMax[1] = boxMax[2] = 1.;
   double cutoff = 0.03;               // 0.012*2.5=0.03; where 2.5 = kernel support radius
   unsigned int rebuildFrequency = 6;  // has to be multiple of two, as there are two functor calls per iteration.
   double skinToCutoffRatio = 0.15;
@@ -38,5 +79,18 @@ int main() {
   sphSystem.setCutoff(cutoff);
   sphSystem.setVerletSkin(skinToCutoffRatio * cutoff);
   sphSystem.setVerletRebuildFrequency(rebuildFrequency);
+
+  std::set<autopas::ContainerOption> allowedContainers{autopas::ContainerOption::linkedCells,
+                                                       autopas::ContainerOption::verletLists,
+                                                       autopas::ContainerOption::verletListsCells};
+  sphSystem.setAllowedContainers(allowedContainers);
+
+  sphSystem.init();
+
+  double dt;
+  double t_end;
+  SetupIC(sphSystem, &t_end, boxMax);
+  Initialize(sphSystem);
+  LogParticlePositions(sphSystem);
 }
 
